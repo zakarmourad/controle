50265a1d766f07bd35b2bfda7315950dcc6562c1
zakarmourad <49688107+zakarmourad@users.noreply.github.com>
2022-05-08 22:54:41 +0000

    commit1

--- controle/zakarmourad/models.py
+++ controle/zakarmourad/models.py
@@ -3,16 +3,23 @@ from django.db import models
 # Create your models here.
 
 
+class Categorie(models.Model):
+    nomCategorie = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.nomCategorie
+
+
 class Produit(models.Model):
     produitRef = models.CharField(max_length=30)
     nomProduit = models.CharField(max_length=30)
     dateProduction = models.DateField()
     prix = models.FloatField()
+    categorie = models.ForeignKey(Categorie, on_delete=models.CASCADE, null=True)
 
+    def __str__(self):
+        return self.nomProduit
 
-class Commande(models.Model):
-    referenceCmd = models.CharField(max_length=30)
-    dateCmd = models.DateField()
 
 
 class Personne(models.Model):
@@ -20,6 +27,16 @@ class Personne(models.Model):
     prenom = models.CharField(max_length=30)
     email = models.CharField(max_length=30)
 
+    def __str__(self):
+        return self.nom
 
-class Categorie(models.Model):
-    nomCategorie = models.CharField(max_length=30)
+
+class Commande(models.Model):
+    referenceCmd = models.CharField(max_length=30)
+    dateCmd = models.DateField()
+    personne = models.ForeignKey(Personne, on_delete=models.CASCADE, null=True)
+    produit = models.ForeignKey(Produit, on_delete=models.CASCADE, null=True)
+
+    def __str__(self):
+        return self.referenceCmd
+
